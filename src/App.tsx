6170b98bf08602fef509aeb2bb5518705d81d092
Alaina Denney <A02241837@aggies.usu.edu>
2025-04-03 16:18:32 -0600

    adding pages to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/mainPage";
+import TermsPage from "./pages/termsOfService";
+import PrivacyPage from "./pages/privacyPolicy";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="terms-of-service" element={<TermsPage />}/>
+        <Route path="privacy-policy" element={<PrivacyPage />}/>
       </Routes>
     </Router>
   );
